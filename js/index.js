c75e6a14eec1e2b74a39c38754fa3dd32c3a560e
maple5233 <326153996@qq.com>
2016-09-24 17:53:36 +0800

    Do some code refactoring

---
+++ js/index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var vm = new Vue ({
+    el: '#HolePage',
+    data: {
+        loginMail : '',
+        loginPassword : '',
+        registMail : '',
+        registPassword : ''
+    },
+    ready() {
+        // console.log(1);
+        setTimeout(function(){
+            document.getElementsByTagName('body')[0].style.height = window.innerHeight+'px';  
+        },20); 
+    },
+    methods: {
+        login : function() {
+            window.location.href = "personalphoto.html";
+        },
+        regist : function() {
+            
+        }
+    }
+})
